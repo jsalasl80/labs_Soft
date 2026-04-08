28cda93058485154fab3ce0dbf0e2e3b09c9ef26
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2026-04-07 19:35:45 -0600

    Agregado funcionalidad completa del lab3

--- Laboratorio3_C37130/frontend-lab/src/App.vue
+++ Laboratorio3_C37130/frontend-lab/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CountriesList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from "./components/CountriesList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CountriesList,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
